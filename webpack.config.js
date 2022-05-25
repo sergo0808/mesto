af2bf200ed9f36d4ddfbce96b4990ccb741dfede
Savitskiy <sovick90@bk.ru>
2022-05-25 09:13:47 +0300

    webpack install

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -27,6 +28,12 @@ module.exports = {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource'
       },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader'
+        }]
+      }
     ]
   },
   plugins: [
@@ -34,5 +41,6 @@ module.exports = {
       template: './src/index.html'
     }),
         new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin()
   ]
 };
