ae47f403ddf999f2c588272a848e5f79059905cc
Savitskiy <sovick90@bk.ru>
2022-05-25 09:04:43 +0300

    webpack install

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,11 @@ module.exports = {
         test: /\.js$/,
         use: 'babel-loader',
         exclude: /node_modules/
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
     ]
   },
   plugins: [
