d7909269ae548a74fe35d905afff6d298e97cdce
Savitskiy <sovick90@bk.ru>
2022-05-25 11:07:58 +0300

    webpack install

--- src/index.js
+++ src/index.js
@@ -1,98 +1,90 @@
-const numbers = [2, 3, 5];
 
-// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
-
-console.log(doubledNumbers); // 4, 6, 10 
 import './pages/index.css'
-// import { initialCards, Card, enableValidation } from "../scripts/cards.js";
-// import { FormValidator } from "../scripts/FormValidator.js";
-// import PopupWithImage from "../scripts/PopupWithImage.js";
-// import PopupWithForm from "../scripts/PopupWithForm.js";
-// import Section from "../scripts/section.js";
-// import { UserInfo } from "../scripts/UserInfo.js";
-// const profileButton = document.querySelector(".profile__info-button"); 
-// export const profileButtonClose = document.querySelector(".popup__close-button-edit"); 
-// const popupFormElement = document.querySelector(".popup__container"); 
-// const profileNameInput = document.querySelector(".popup__input_name_active"); 
-// const profileJobInput = document.querySelector(".popup__input_job_active"); 
-// const profileInfoName = document.querySelector(".profile__info-name"); 
-// const profileInfoJob = document.querySelector(".profile__info-job");
-// const cardsButtonAdd = document.querySelector('.profile__add-button'); 
-// const cardsButtonClose = document.querySelector(".close-button-add"); 
-// const popupFormElementAdd = document.querySelector(".popup__container-add"); 
-// const nameInputAdd = document.querySelector(".popup__input_name_add"); 
-// const linkInputAdd = document.querySelector(".popup__input_link_add");
-// const containerElement = document.querySelector('.elements'); 
-// const popupImg = document.querySelector('.popup_type_image'); 
-// const formElementImg = popupImg.querySelector('.popup__container_image'); 
-// export const imgButtonClose = popupImg.querySelector(".popup__close-button_image"); 
-// export const imgElement = popupImg.querySelector(".popup__image"); 
-// export const popupCaption = popupImg.querySelector(".popup__caption");
-// const popups = Array.from(document.querySelectorAll('.popup'));
-// const profileFormValidator = new FormValidator(enableValidation, popupFormElement);
-// const addFormValidator =  new FormValidator(enableValidation, popupFormElementAdd);
+import { initialCards, Card, enableValidation } from "../scripts/cards.js";
+import { FormValidator } from "../scripts/FormValidator.js";
+import PopupWithImage from "../scripts/PopupWithImage.js";
+import PopupWithForm from "../scripts/PopupWithForm.js";
+import Section from "../scripts/section.js";
+import { UserInfo } from "../scripts/UserInfo.js";
+const profileButton = document.querySelector(".profile__info-button"); 
+export const profileButtonClose = document.querySelector(".popup__close-button-edit"); 
+const popupFormElement = document.querySelector(".popup__container"); 
+const profileNameInput = document.querySelector(".popup__input_name_active"); 
+const profileJobInput = document.querySelector(".popup__input_job_active"); 
+const profileInfoName = document.querySelector(".profile__info-name"); 
+const profileInfoJob = document.querySelector(".profile__info-job");
+const cardsButtonAdd = document.querySelector('.profile__add-button'); 
+const cardsButtonClose = document.querySelector(".close-button-add"); 
+const popupFormElementAdd = document.querySelector(".popup__container-add"); 
+const nameInputAdd = document.querySelector(".popup__input_name_add"); 
+const linkInputAdd = document.querySelector(".popup__input_link_add");
+const containerElement = document.querySelector('.elements'); 
+const popupImg = document.querySelector('.popup_type_image'); 
+const formElementImg = popupImg.querySelector('.popup__container_image'); 
+export const imgButtonClose = popupImg.querySelector(".popup__close-button_image"); 
+export const imgElement = popupImg.querySelector(".popup__image"); 
+export const popupCaption = popupImg.querySelector(".popup__caption");
+const popups = Array.from(document.querySelectorAll('.popup'));
+const profileFormValidator = new FormValidator(enableValidation, popupFormElement);
+const addFormValidator =  new FormValidator(enableValidation, popupFormElementAdd);
 
-// console.log('Hello, World!') 
+console.log('Hello, World!') 
 
-// const cardList = new Section({
-//   items: initialCards,
-//   renderer: (item) => {
-//     createCard(item); 
-//   }
-// }, containerElement);
-// cardList.renderItems();
+const cardList = new Section({
+  items: initialCards,
+  renderer: (item) => {
+    createCard(item); 
+  }
+}, containerElement);
+cardList.renderItems();
 
-// const popupEdit = new PopupWithForm('.popup_type_edit', saveDataFormEdit)
+const popupEdit = new PopupWithForm('.popup_type_edit', saveDataFormEdit)
 
-// function saveDataFormEdit(evt) { 
-//   UserData.setUserInfo(profileNameInput, profileJobInput);
-//   popupEdit.close(); 
-// };
+function saveDataFormEdit(evt) { 
+  UserData.setUserInfo(profileNameInput, profileJobInput);
+  popupEdit.close(); 
+};
 
-// const UserData = new UserInfo({
-//   name: profileInfoName,
-//   job: profileInfoJob
-
-// кроме изменения синтаксиса пропали пробелы, а имена переменных сократились до одной буквы
-
-// });
+const UserData = new UserInfo({
+  name: profileInfoName,
+  job: profileInfoJob
+});
 
 
-// const popupAdd = new PopupWithForm('.popup_type_add', addCard)
+const popupAdd = new PopupWithForm('.popup_type_add', addCard)
 
-// function createCard(item){
-//   const card = new Card(item,'#element-teamplate', {
-//     handleOpenCard: () => {
-//       const imgPopup = new PopupWithImage(item, '.popup_type_image')
-//       imgPopup.open();
-//       imgPopup.setEventListeners();   
-//     }        
-//   })
-//   const cardElement = card.generateCard();
-//   cardList.addItem(cardElement);  
-// }
+function createCard(item){
+  const card = new Card(item,'#element-teamplate', {
+    handleOpenCard: () => {
+      const imgPopup = new PopupWithImage(item, '.popup_type_image')
+      imgPopup.open();
+      imgPopup.setEventListeners();   
+    }        
+  })
+  const cardElement = card.generateCard();
+  cardList.addItem(cardElement);  
+}
 
-// function addCard(item) {
-//   createCard(item);
-//   popupAdd.close();
+function addCard(item) {
+  createCard(item);
+  popupAdd.close();
   
-// }
+}
 
-// profileButton.addEventListener("click", function(){
-//   const data = UserData.getUserInfo();
-//   profileNameInput.value = data.name; 
-//   profileJobInput.value = data.job; 
-//   profileFormValidator.clearError();
-//   popupEdit.open();
-// }); 
+profileButton.addEventListener("click", function(){
+  const data = UserData.getUserInfo();
+  profileNameInput.value = data.name; 
+  profileJobInput.value = data.job; 
+  profileFormValidator.clearError();
+  popupEdit.open();
+}); 
 
-// cardsButtonAdd.addEventListener("click", function(){
-//   addFormValidator.clearError();
-//   popupAdd.open();
-// });
+cardsButtonAdd.addEventListener("click", function(){
+  addFormValidator.clearError();
+  popupAdd.open();
+});
 
-// profileFormValidator.enableValidation();
-// addFormValidator.enableValidation();
-// popupEdit.setEventListeners();
-// popupAdd.setEventListeners();
+profileFormValidator.enableValidation();
+addFormValidator.enableValidation();
+popupEdit.setEventListeners();
+popupAdd.setEventListeners();
