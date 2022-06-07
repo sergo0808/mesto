100e5567e0174d882cd155f9e765d38eac481671
Savitskiy <sovick90@bk.ru>
2022-06-07 11:30:34 +0300

    hot fix

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,18 @@
 import { userId } from "../pages/index.js";
 class Card {
-  constructor({name, link, owner, _id, likes}, cardSelector, handleOpenCard, handleDeleteCard, handleLikeClick, handleLikeClickDrop){
+  constructor(
+    { name, link, owner, _id, likes },
+    cardSelector,
+    handleOpenCard,
+    handleDeleteCard,
+    handleLikeClick,
+    handleLikeClickDrop
+  ) {
     this._link = link;
     this._name = name;
     this._id = _id;
-    this._owner = owner
-    this._likes = likes
+    this._owner = owner;
+    this._likes = likes;
     this._cardSelector = cardSelector;
     this._handleOpenCard = handleOpenCard;
     this._handleDeleteCard = handleDeleteCard;
@@ -13,53 +20,60 @@ class Card {
     this._handleLikeClickDrop = handleLikeClickDrop;
     this._isLike = false;
   }
- 
   _setEventListeners() {
-    this._element.querySelector('.element__group-like').addEventListener('click', (evt) => {
-      evt.target.classList.toggle('element__group-like_active')});
-      console.log(!this._isLike);
-      if (!this._isLike) {
-        this._handleLikeClick( this )
-        this._isLike = true;
-      } else {
-        this._handleLikeClickDrop( this )
-        this._isLike = false;
-      } 
-
-    this._element.querySelector('.element__group-basket').addEventListener('click', () => {  this._handleDeleteCard(this._id)});
-    this._element.querySelector('.element__mask-group').addEventListener('click', () => {this._handleOpenCard(this._name, this._link)});
-  };
-
- 
+    this._element
+      .querySelector(".element__group-like")
+      .addEventListener("click", (evt) => {
+        evt.target.classList.toggle("element__group-like_active");
+      });
+    if (!this._isLike) {
+      this._handleLikeClick(this);
+      this._isLike = true;
+    } else {
+      this._handleLikeClickDrop(this);
+      this._isLike = false;
+    }
+    this._element
+      .querySelector(".element__group-basket")
+      .addEventListener("click", () => {
+        this._handleDeleteCard(this._id);
+      });
+    this._element
+      .querySelector(".element__mask-group")
+      .addEventListener("click", () => {
+        this._handleOpenCard(this._name, this._link);
+      });
+  }
   handleBasketClick() {
-    this._element.remove();  
+    this._element.remove();
     this._element = null;
-  };
- 
-  _getTemplate(){
+  }
+  _getTemplate() {
     const cardElement = document
-    .querySelector(this._cardSelector)
-    .content.querySelector('.element')
-    .cloneNode(true);
+      .querySelector(this._cardSelector)
+      .content.querySelector(".element")
+      .cloneNode(true);
     return cardElement;
-  };
-
+  }
   generateCard() {
     this._element = this._getTemplate();
-    this._element.querySelector('.element__group-text').textContent = this._name;
-    const cardImg = this._element.querySelector('.element__mask-group');
+    this._element.querySelector(".element__group-text").textContent =
+      this._name;
+    const cardImg = this._element.querySelector(".element__mask-group");
     cardImg.src = this._link;
     cardImg.id = this._id;
     cardImg.alt = this._name;
     cardImg.owner = this._owner;
-    cardImg.likes = this._likes; 
+    cardImg.likes = this._likes;
     if (cardImg.owner._id === userId) {
-      this._element.querySelector('.element__group-basket').classList.add('element__group-basket_active')
-    };
-    this._element.querySelector('.element__group-count').textContent = cardImg.likes.length;
+      this._element
+        .querySelector(".element__group-basket")
+        .classList.add("element__group-basket_active");
+    }
+    this._element.querySelector(".element__group-count").textContent =
+      cardImg.likes.length;
     this._setEventListeners();
-    return this._element
-  } 
-};
-
-export {Card};
+    return this._element;
+  }
+}
+export { Card };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,141 +1,141 @@
-import '../pages/index.css'
+import "../pages/index.css";
 import { Card } from "../components/Cards.js";
-import { FormValidator, enableValidation} from "../components/FormValidator.js";
+import {
+  FormValidator,
+  enableValidation,
+} from "../components/FormValidator.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import Section from "../components/Section.js";
 import { UserInfo } from "../components/UserInfo.js";
-import { Api } from '../components/Api';
-import { PopupWithConfirm } from '../components/PopupWithConfirm';
-import { data } from 'autoprefixer';
-const profileButton = document.querySelector(".profile__info-button"); 
-export const profileButtonClose = document.querySelector(".popup__close-button-edit"); 
-const popupFormElement = document.querySelector(".popup__container"); 
-const profileNameInput = document.querySelector(".popup__input_name_active"); 
-const profileJobInput = document.querySelector(".popup__input_job_active"); 
-const profileInfoName = document.querySelector(".profile__info-name"); 
+import { Api } from "../components/Api";
+import { PopupWithConfirm } from "../components/PopupWithConfirm";
+import { data } from "autoprefixer";
+const profileButton = document.querySelector(".profile__info-button");
+export const profileButtonClose = document.querySelector(
+  ".popup__close-button-edit"
+);
+const popupFormElement = document.querySelector(".popup__container");
+const profileNameInput = document.querySelector(".popup__input_name_active");
+const profileJobInput = document.querySelector(".popup__input_job_active");
+const profileInfoName = document.querySelector(".profile__info-name");
 const profileInfoJob = document.querySelector(".profile__info-job");
-const cardsButtonAdd = document.querySelector('.profile__add-button'); 
-const popupFormElementAdd = document.querySelector(".popup__container-add"); 
-export const imgButtonClose = document.querySelector(".popup__close-button_image"); 
-export const imgElement = document.querySelector(".popup__image"); 
+const cardsButtonAdd = document.querySelector(".profile__add-button");
+const popupFormElementAdd = document.querySelector(".popup__container-add");
+export const imgButtonClose = document.querySelector(
+  ".popup__close-button_image"
+);
+export const imgElement = document.querySelector(".popup__image");
 export const popupCaption = document.querySelector(".popup__caption");
-const profileFormValidator = new FormValidator(enableValidation, popupFormElement);
-const addFormValidator =  new FormValidator(enableValidation, popupFormElementAdd);
+const profileFormValidator = new FormValidator(
+  enableValidation,
+  popupFormElement
+);
+const addFormValidator = new FormValidator(
+  enableValidation,
+  popupFormElementAdd
+);
 const userData = new UserInfo(profileInfoName, profileInfoJob);
-const imgPopup = new PopupWithImage('.popup_type_image')
-const popupAdd = new PopupWithForm('.popup_type_add', addCard)
-const popupEdit = new PopupWithForm('.popup_type_edit', saveDataFormEdit)
-const popupConfirm = new PopupWithConfirm('.popup_type_confirm')
-
+const imgPopup = new PopupWithImage(".popup_type_image");
+const popupAdd = new PopupWithForm(".popup_type_add", addCard);
+const popupEdit = new PopupWithForm(".popup_type_edit", saveDataFormEdit);
+const popupConfirm = new PopupWithConfirm(".popup_type_confirm");
 const config = {
-  url: 'https://mesto.nomoreparties.co/v1/cohort-42',
+  url: "https://mesto.nomoreparties.co/v1/cohort-42",
   headers: {
-    authorization: '5a769756-07f6-441a-bbc2-3fc7a58dd4ed',
-    'Content-Type': 'application/json',
-  }
-}
-
+    authorization: "5a769756-07f6-441a-bbc2-3fc7a58dd4ed",
+    "Content-Type": "application/json",
+  },
+};
 const api = new Api(config);
-let cardList = {}
-api.getInitialCards()
-  .then(data => {
-    console.log(data);
-    cardList = new Section({
+let cardList = {};
+api.getInitialCards().then((data) => {
+  console.log(data);
+  cardList = new Section(
+    {
       items: data,
       renderer: (item) => {
-        createCard(item); 
-      }
-    }, '.elements');
-    cardList.renderItems();
-  });
+        createCard(item);
+      },
+    },
+    ".elements"
+  );
+  cardList.renderItems();
+});
+let userId;
+api.UserInfo().then((data) => {
+  profileInfoName.textContent = data.name;
+  profileInfoJob.textContent = data.about;
+  userId = data._id;
+});
 
-  let userId;
-
-  api.UserInfo()
-  .then(data => {
-    profileInfoName.textContent = data.name
-    profileInfoJob.textContent = data.about
-    userId = data._id
-})
-
-function createCard(item) {  
-  const card = new Card(item, "#element-teamplate", handleOpenCard,  handleDeleteCard, handleLikeClick, handleLikeClickDrop);
+function createCard(item) {
+  const card = new Card(
+    item,
+    "#element-teamplate",
+    handleOpenCard,
+    handleDeleteCard,
+    handleLikeClick,
+    handleLikeClickDrop
+  );
   const cardElement = card.generateCard();
   cardList.addItem(cardElement);
 }
-
 const handleLikeClick = (item) => {
-  api.likeCardApi(item._id)
-    .then((res) => { 
-      let count = item._element.querySelector('.element__group-count')
-      count.textContent = res.likes.length;       
-  })
+  api.likeCardApi(item._id).then((res) => {
+    let count = item._element.querySelector(".element__group-count");
+    count.textContent = res.likes.length;
+  });
 };
-
 const handleLikeClickDrop = (item) => {
-  api.likeCardApiDrop(item._id)
-    .then((res) => { 
-      let count = item._element.querySelector('.element__group-count')
-      count.textContent = res.likes.length;       
-  })
+  api.likeCardApiDrop(item._id).then((res) => {
+    let count = item._element.querySelector(".element__group-count");
+    count.textContent = res.likes.length;
+  });
 };
 
-
-
-
-   function handleDeleteCard(id) {   
-    popupConfirm.open();
-    popupConfirm.deleteConfirm(   
-      api.deleteCardApi(id)
-        .then(id => {
-        console.log('DELETE') 
-        handleBasketClick();
+function handleDeleteCard(id) {
+  popupConfirm.open();
+  popupConfirm.deleteConfirm(
+    api.deleteCardApi(id).then((id) => {
+      console.log("DELETE");
+      handleBasketClick();
     })
-    );
-    
-  }
+  );
+}
 
-  function addCard(input) {
-    api.addCardApi(input)
-    .then(input => {
-      createCard(input);
-      popupAdd.close();
-      
-    }) 
-  };
+function addCard(input) {
+  api.addCardApi(input).then((input) => {
+    createCard(input);
+    popupAdd.close();
+  });
+}
 
-  function saveDataFormEdit(input) { 
-    api.updateUserInfom(input)
-     .then(input => {
-       userData.setUserInfo(input.name, input.about);
-       popupEdit.close();
-     })
-  };
-
-profileButton.addEventListener("click", function(){
-const user = userData.getUserInfo();
-profileNameInput.value = user.name; 
-profileJobInput.value = user.about; 
-profileFormValidator.clearError();
-popupEdit.open();
-});   
-  
-cardsButtonAdd.addEventListener("click", function(){
+function saveDataFormEdit(input) {
+  api.updateUserInfom(input).then((input) => {
+    userData.setUserInfo(input.name, input.about);
+    popupEdit.close();
+  });
+}
+profileButton.addEventListener("click", function () {
+  const user = userData.getUserInfo();
+  profileNameInput.value = user.name;
+  profileJobInput.value = user.about;
+  profileFormValidator.clearError();
+  popupEdit.open();
+});
+cardsButtonAdd.addEventListener("click", function () {
   addFormValidator.clearError();
   popupAdd.open();
 });
 
-
 function handleOpenCard(name, link) {
   imgPopup.open(name, link);
-};
-
+}
 profileFormValidator.enableValidation();
 addFormValidator.enableValidation();
 popupEdit.setEventListeners();
 popupAdd.setEventListeners();
 imgPopup.setEventListeners();
-popupConfirm.setEventListeners(); 
-
-export {userId}
+popupConfirm.setEventListeners();
+export { userId };
